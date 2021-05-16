c6183d421a23babe2a6b0be10ee850b5d4e67d31
miho <ilovemyroro624@gmail.com>
2021-05-16 22:01:23 +0900

    changed appearance to chart type

--- src/components/user.svelte
+++ src/components/user.svelte
@@ -6,185 +6,64 @@
 </script>
 
 <div id="user" transition:slide="{{delay: 250, duration: 1000, easing: quintOut}}">
-    <div id="container">
-        <div id="base">
-            <div id="name">{name}</div>
-            <div id="balance">${balance.toLocaleString("ko-KR")}</div>
-        </div>
-        <div id="desc">
-            <div id="primary">
-                <div id="aircraft">
-                    {aircraft}
-                </div>
-                <div id="sortie">
-                    {sortie} TIMES ACTION
-                </div>
-            </div>
-            <div id="secondary">
-                <div id="air" class="sec-data">
-                    AIR: {air}
-                </div>
-                <div id="ground" class="sec-data">
-                    GND: {ground}
-                </div>
-                <div id="killed" class="sec-data">
-                    KIA: {killed}
-                </div>
-            </div>
-        </div>
-    </div>
+    <ul>
+        <li id="name">
+            {name}
+        </li>
+        <li style="color: #FFD700;">
+            ${balance.toLocaleString("ko-KR")}
+        </li>
+        <li>
+            {aircraft}
+        </li>
+        <li>
+            {sortie}
+        </li>
+        <li>
+            {air}
+        </li>
+        <li>
+            {ground}
+        </li>
+        <li style="color: #FF0000;">
+            {killed}
+        </li>
+    </ul>
 </div>
 
 <style>
     #user {
+        font-family: "Ubuntu Mono", monospace;
         background-color: #0A0E12;
-        justify-content: center;
-        border-radius: 10px;
-        align-items: center;
         position: relative;
-        display: flex;
-        height: 200px;
-        margin: 20px;
+        font-size: 1.25rem;
+        height: 50px;
+        margin: 5px;
     }
 
-    #container {
+    ul {
+        display: grid;
+        grid-template-columns: repeat(7, 1fr);
+        grid-template-rows: 100%;
+        position: relative;
         width: 100%;
         height: 100%;
-        position: relative;
-        display: grid;
-        grid-template-columns: 40% 60%;
-        grid-template-rows: 100%;
     }
 
-    #base {
-        display: grid;
-        grid-template-columns: 100%;
-        grid-template-rows: 50% 50%;
-        background: linear-gradient(135deg, #C33864 0%, #822A99 50%, #1E2670 100%);
-        border-top-left-radius: 10px;
-        border-bottom-left-radius: 10px;
-        text-shadow: 1px 3px 3px black;
-    }
-
-    #desc {
-        display: grid;
-        grid-template-columns: 75% 25%;
-        grid-template-rows: 100%;
-        font-family: "Ubuntu Mono", monospace;
+    li {
+        list-style: none;
+        margin: auto;
     }
 
     #name {
-        font-size: 2.5rem;
-        font-weight: 700;
-        margin: auto 0;
-        padding: 0 50px;
-    }
-
-    #balance {
-        font-size: 4rem;
-        font-weight: 700;
-        color: #FFD700;
-        margin: auto 0;
-        text-align: right;
-        padding: 0 50px;
-    }
-
-    #aircraft {
-        font-size: 3.5rem;
-        font-weight: 700;
-        text-align: center;
-        padding: 1rem;
-    }
-
-    #sortie {
-        font-size: 1.75rem;
-        font-weight: 700;
-        letter-spacing: 1px;
-        color: #ADFF2F;
-    }
-
-    #killed {
-        color: #FF0000;
-    }
-
-    #primary {
+        background: linear-gradient(135deg, #C33864 0%, #822A99 50%, #1E2670 100%);
+        height: 100%;
+        width: 100%;
         display: flex;
-        flex-direction: column;
         align-items: center;
         justify-content: center;
-    }
-
-    #secondary {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        justify-content: space-around;
-        font-size: 2rem;
-        text-transform: uppercase;
-        letter-spacing: 2px;
+        font-family: "Noto Sans KR", sans-serif;
         font-weight: 700;
-    }
-
-    @media only screen and (max-width: 1600px) {
-        #container {
-            grid-template-columns: 100%;
-            grid-template-rows: 25% 75%;
-        }
-
-        #base {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            padding: 0 30px;
-            background: linear-gradient(135deg, #C33864 0%, #822A99 50%, #1E2670 100%);
-            border-top-right-radius: 10px;
-            border-bottom-left-radius: 0;
-        }
-
-        #desc {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            padding: 0 30px;
-        }
-
-        #name {
-            font-size: 1rem;
-            font-weight: 700;
-            margin: 0;
-            padding: 0;
-        }
-
-        #balance {
-            font-size: 1.25rem;
-            font-weight: 700;
-            color: #FFD700;
-            margin: 0;
-            text-align: unset;
-            padding: 0;
-        }
-
-        #aircraft {
-            font-size: 1.5rem;
-            padding: 5px 0;
-        }
-
-        #sortie {
-            font-size: 1rem;
-            letter-spacing: 0;
-        }
-
-        #primary {
-            align-items: flex-start;
-        }
-
-        #secondary {
-            font-size: 1rem;
-            font-weight: 700;
-        }
-
-        .sec-data {
-            padding: 5px 0;
-        }
+        font-size: 1.25rem;
     }
 </style>
--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -8,6 +8,17 @@
 </script>
 
 <div id="home">
+    <div id="field">
+        <ul>
+            <li>Player</li>
+            <li>Balance</li>
+            <li>Aircraft</li>
+            <li>Sortie</li>
+            <li>Air</li>
+            <li>Ground</li>
+            <li>Killed</li>
+        </ul>
+    </div>
     {#await getData() then a}
         {#each a as v}
             <User name={v.Name}
@@ -21,3 +32,29 @@
         {/each}
     {/await}
 </div>
+
+<style>
+    #field {
+        font-family: "Ubuntu Mono", monospace;
+        background-color: #0A0E12;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        font-size: 1.25rem;
+        position: relative;
+        height: 50px;
+        width: 100%;
+    }
+
+    ul {
+        display: grid;
+        grid-template-columns: repeat(7, 1fr);
+        grid-template-rows: 100%;
+        width: 100%;
+        height: 100%;
+    }
+
+    li {
+        list-style: none;
+        margin: auto;
+    }
+</style>
